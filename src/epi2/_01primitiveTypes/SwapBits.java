9ea7ee743915f4b9ec0f8c52d500180555e1f68e
Christian Rubiales <christianrubiales@yahoo.com>
2017-09-01 18:20:23 +0800

    SwapBits

---
+++ src/epi2/_01primitiveTypes/SwapBits.java
@@ -0,0 +1,59 @@
+package epi2._01primitiveTypes;
+
+public class SwapBits {
+
+	static long swapBits(long n, int i, int j) {
+		short bi = getBitAt(n, i);
+		short bj = getBitAt(n, j);
+		
+		if (bi != bj) {
+			n = setBitAt(n, i, bj);
+			n = setBitAt(n, j, bi);
+		}
+		
+		return n;
+	}
+	
+	static short getBitAt(long n, int i) {
+		n >>>= i;
+		return (short) (n & 1);
+	}
+
+	static long setBitAt(long n, int i, int b) {
+		long mask = 1L << i;
+		return n ^ mask;
+	}
+
+	// slightly more efficient
+	static long swapBitsEPI(long n, int i, int j) {
+		
+		// get ith and jth bits and check if different
+		if (((n >>> i) & 1) != (((n >>> j) & 1))) {
+			
+			// swap bits by flipping using xor
+			// 1^1=0 and 0^1=1
+			long mask = (1L << i) | (1L << j);
+			n ^= mask;
+		}
+		return n;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(getBitAt(0b101, 0));//1
+		System.out.println(getBitAt(0b101, 1));//0
+		System.out.println(getBitAt(0b101, 2));//1
+		System.out.println();
+		
+		System.out.println(setBitAt(0b101, 0, 0));//4
+		System.out.println(setBitAt(0b101, 1, 1));//7
+		System.out.println();
+		
+		System.out.println(swapBits(0b101, 0, 1));//6
+		System.out.println(swapBits(0b101, 1, 2));//3
+		System.out.println();
+		
+		System.out.println(swapBitsEPI(0b101, 0, 1));//6
+		System.out.println(swapBitsEPI(0b101, 1, 2));//3
+	}
+
+}
