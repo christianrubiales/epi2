3c42390b9a712aaf4e88e57e418f73c960be2cc4
Christian Rubiales <christianrubiales@yahoo.com>
2017-09-01 16:24:41 +0800

    sort5

--- src/epi2/_00bonus/Sort5.java
+++ src/epi2/_00bonus/Sort5.java
@@ -6,7 +6,9 @@ import java.util.Arrays;
  * Given 25 DISTINCT numbers, return top 3 while minimizing use of sort5.
  * Page 32-33
  * Form groups of 5 and sort the 5 groups, then sort the top from the groups.
- * Minimum 6 sort5 uses.
+ * Minimum 6 sort5 uses according to book.
+ * 
+ * Problem is similar to top 3 of 25 horses, minimum is 7 races.
  */
 public class Sort5 {
 	
@@ -23,7 +25,7 @@ public class Sort5 {
 			a[i] = sort5(Arrays.copyOfRange(array, (i*5), (i*5) + 5)); // sort the range
 			x[i] = a[i][4]; // copy the top in the sorted range
 		}
-		Arrays.sort(x); // sort the top from the groups
+		sort5(x); // sort the top from the groups
 		
 		return new int[] {x[4], x[3], x[2]};
 	}
@@ -43,6 +45,10 @@ public class Sort5 {
 	public static void main(String[] args) {
 		int[] array = new int[] {3, 13, 12, 23, 1, 7, 4, 5, 6, 15, 22, 19, 10, 17, 9, 8, 11, 18, 25, 2, 16, 24, 14, 20, 21};
 		print(array, top3(array)); // 25,24,23
+		
+		// COUNTER-EXAMPLE to 6 sorts: 1 group contains the top 3
+		array = new int[] {25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
+		print(array, top3(array)); // 25, 20, 15 -> wrong
 	}
 
 }
