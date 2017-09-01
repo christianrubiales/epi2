a92a19c92f4bb61d3f5e42f04c9871dedb18de2b
Christian Rubiales <christianrubiales@yahoo.com>
2017-09-01 16:32:26 +0800

    count bits

--- src/epi2/_01primitiveTypes/CountBits.java
+++ src/epi2/_01primitiveTypes/CountBits.java
@@ -3,6 +3,7 @@ package epi2._01primitiveTypes;
 public class CountBits {
 	
 	// n & 1 equals either 1 or 0
+	// O(n), n is number of bits
 	static short countBits(int n) {
 		short bits = 0;
 		
@@ -13,10 +14,25 @@ public class CountBits {
 		
 		return bits;
 	}
+	
+	// O(k), k is the number of 1 bits, not all the bits
+	static short countBitsByClearingLowestBit(int n) {
+		short bits = 0;
+		
+		while (n != 0) {
+			bits++;
+			n &= n-1;
+		}
+		
+		return bits;
+	}
 
 	public static void main(String[] args) {
 		System.out.println(countBits(12));//2
 		System.out.println(countBits(7));//3
+
+		System.out.println(countBitsByClearingLowestBit(12));//2
+		System.out.println(countBitsByClearingLowestBit(7));//3
 	}
 
 }
