7678b7bfa3da5a9d64964ff83bb0cdfe3d4d32f1
Christian Rubiales <christianrubiales@yahoo.com>
2017-09-04 02:13:51 +0800

    EvenOddPartitioning

--- src/epi2/_02arrays/EvenOddPartitioning.java
+++ src/epi2/_02arrays/EvenOddPartitioning.java
@@ -5,21 +5,24 @@ import java.util.Arrays;
 public class EvenOddPartitioning {
 	
 	// use two pointers, swapping the front entry with the back if not even
+	// O(n)
 	static int[] evenOddPartition(int[] b) {
 		int[] a = Arrays.copyOf(b, b.length);
-		int i = 0;
-		int j = a.length - 1;
+		int i = 0; // front pointer
+		int j = a.length - 1; // back pointer
 		
 		while (i < j) {
-			// swap with back if not even
+			// swap with back pointer if front pointer is not even
 			if (a[i] % 2 != 0) {
 				int t = a[i];
 				a[i] = a[j];
 				a[j] = t;
 			}
 			
+			// always decrement back pointer
 			j--;
 			
+			// increment front pointer if even
 			if (a[i] % 2 == 0) {
 				i++;
 			}
@@ -42,6 +45,9 @@ public class EvenOddPartitioning {
 	
 	public static void main(String[] args) {
 		print(new int[] {1,2,3,4,5,6,7,8,9}, evenOddPartition(new int[] {1,2,3,4,5,6,7,8,9}));
+		print(new int[] {1,3,5,7,9}, evenOddPartition(new int[] {1,3,5,7,9}));
+		print(new int[] {2,4,6,8,1,3,5,7,9}, evenOddPartition(new int[] {2,4,6,8,1,3,5,7,9}));
+		print(new int[] {2,4,6,8}, evenOddPartition(new int[] {2,4,6,8}));
 	}
 
 }
