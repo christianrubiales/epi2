553edcb3753295af4952cf4707cb0900b9281cd0
Christian Rubiales <christianrubiales@yahoo.com>
2017-09-07 00:45:09 +0800

    RandomPermutation

---
+++ src/epi2/_02arrays/RandomPermutation.java
@@ -0,0 +1,33 @@
+package epi2._02arrays;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class RandomPermutation {
+	
+	// create the initial permutation from 0 to n-1
+	// then shuffle the permutation
+	static Integer[] randomPermutation(int n) {
+		// create the initial permutation
+		Integer[] a = new Integer[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = i;
+		}
+		
+		// shuffle the permutation
+		Random random = new Random();
+		for (int i = n; i > 1; i--) {
+			int r = random.nextInt(i);
+			Integer t = a[i-1];
+			a[i-1] = a[r];
+			a[r] = t;
+		}
+		
+		return a;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.deepToString(randomPermutation(6)));
+	}
+
+}
