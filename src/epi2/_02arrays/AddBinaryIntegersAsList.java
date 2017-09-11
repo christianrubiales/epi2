9f5e9b4f10a5fe444cd92d00cc19d578fe198086
Christian Rubiales <christianrubiales@yahoo.com>
2017-09-12 02:28:12 +0800

    AddBinaryIntegersAsList

--- src/epi2/_02arrays/AddBinaryIntegersAsList.java
+++ src/epi2/_02arrays/AddBinaryIntegersAsList.java
@@ -7,10 +7,9 @@ import java.util.List;
 public class AddBinaryIntegersAsList {
 	
 	static List<Integer> add(List<Integer> a, List<Integer> b) {
-		List<Integer> c = new ArrayList(a.size() >= b.size() ? a : b);
-		List<Integer> d = new ArrayList(a.size() < b.size() ? a : b);
+		List<Integer> c = new ArrayList<>(a.size() >= b.size() ? a : b);
+		List<Integer> d = new ArrayList<>(a.size() < b.size() ? a : b);
 		int carry = 0;
-		int diff = c.size() - d.size();
 		
 		// add up to the digit of the smaller number
 		for (int i = 0; i < d.size(); i++) {
@@ -51,6 +50,34 @@ public class AddBinaryIntegersAsList {
 		
 		return c;
 	}
+	
+	static List<Integer> add2(List<Integer> a, List<Integer> b) {
+		List<Integer> c = new ArrayList<>(a.size() >= b.size() ? a : b);
+		List<Integer> d = new ArrayList<>(a.size() < b.size() ? a : b);
+		int diff = c.size() - d.size();
+		int carry = 0;
+		
+		// add up to the digit of the smaller number
+		for (int i = d.size() - 1; i > -1; i--) {
+			int sum = c.get(diff + i) + d.get(i) + carry;
+			c.set(diff + i, sum % 2);
+			carry = sum / 2;
+		}
+
+		// add the carry to the remaining digits of the larger number
+		for (int i = diff - 1; i > -1; i--) {
+			int sum = c.get(i) + carry;
+			c.set(i, sum % 2);
+			carry = sum / 2;
+		}
+		
+		// if there is a last carry, the list has to expand
+		if (carry == 1) {
+			c.add(0, 1);
+		}
+		
+		return c;
+	}
 
 	public static void main(String[] args) {
 		System.out.println(add(new ArrayList<>(Arrays.asList(1)), new ArrayList<>(Arrays.asList(0))));//1
@@ -58,6 +85,12 @@ public class AddBinaryIntegersAsList {
 		System.out.println(add(new ArrayList<>(Arrays.asList(1,1)), new ArrayList<>(Arrays.asList(1))));//1,0,0
 		System.out.println(add(new ArrayList<>(Arrays.asList(1,0,1,1)), new ArrayList<>(Arrays.asList(1))));//1,1,0,0
 		System.out.println(add(new ArrayList<>(Arrays.asList(1,0,0,0,0,1,1)), new ArrayList<>(Arrays.asList(1))));//1,0,0,0,1,0,0
-	}
+	
+		System.out.println(add2(new ArrayList<>(Arrays.asList(1)), new ArrayList<>(Arrays.asList(0))));//1
+		System.out.println(add2(new ArrayList<>(Arrays.asList(1)), new ArrayList<>(Arrays.asList(1))));//1,0
+		System.out.println(add2(new ArrayList<>(Arrays.asList(1,1)), new ArrayList<>(Arrays.asList(1))));//1,0,0
+		System.out.println(add2(new ArrayList<>(Arrays.asList(1,0,1,1)), new ArrayList<>(Arrays.asList(1))));//1,1,0,0
+		System.out.println(add2(new ArrayList<>(Arrays.asList(1,0,0,0,0,1,1)), new ArrayList<>(Arrays.asList(1))));//1,0,0,0,1,0,0
+		}
 
 }
