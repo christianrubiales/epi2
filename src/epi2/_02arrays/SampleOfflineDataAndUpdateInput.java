f2096c61a8237a5e1d8339d8be1c8ce96571be82
Christian Rubiales <christianrubiales@yahoo.com>
2017-09-15 00:37:09 +0800

    SampleOfflineDataAndUpdateInput

---
+++ src/epi2/_02arrays/SampleOfflineDataAndUpdateInput.java
@@ -0,0 +1,26 @@
+package epi2._02arrays;
+
+import java.util.Arrays;
+import java.util.Random;
+
+// the first k elements must be the random sample
+public class SampleOfflineDataAndUpdateInput {
+	
+	// Fisher-Yates left to right instead of right to left
+	static Integer[] randomSample(Integer[] data, int k) {
+		Random random = new Random();
+		for (int i = 0; i < k; i++) {
+			int r = i + random.nextInt(data.length - i);
+			int t = data[i];
+			data[i] = data[r];
+			data[r] = t;
+		}
+		
+		return data;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.deepToString(randomSample(new Integer[] {1,2,3,4,5,6}, 3)));
+	}
+
+}
