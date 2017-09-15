1e49c6c2f667b9376d6eab23af74222120fc0a07
Christian Rubiales <christianrubiales@yahoo.com>
2017-09-16 00:36:19 +0800

    DivisionWithoutDivisionMultiplicationModOperators

--- src/epi2/_01primitiveTypes/DivisionWithoutDivisionMultiplicationModOperators.java
+++ src/epi2/_01primitiveTypes/DivisionWithoutDivisionMultiplicationModOperators.java
@@ -28,33 +28,86 @@ public class DivisionWithoutDivisionMultiplicationModOperators {
 			dividend = -dividend;
 		}
 
-		// 3) shift 32 times
-		int result = 0;
-		for (int i = 31; i > -1; i--) {
-			if ((dividend >>> i) >= divisor) {
-				result = (result << 1) | 1;
-				dividend -= divisor << i;
-			} else {
-				result <<= 1;
+		// 3) shift 31 times
+		int quotient = 0;
+		int remainder = 0;
+		int bit = 0;
+		
+		for (int i = 31 - 1; i > -1; i--) { // number of bits in positive word - 1
+			remainder <<= 1;
+			bit = (dividend >>> i) & 1; // get the bit i of the dividend
+			remainder |= bit; // set the retrieved bit to the least significant bit of remainder
+			
+			if (remainder >= divisor) {
+				remainder -= divisor;
+				quotient |= 1 << i; // set the ith bit of the quotient to 1
 			}
 		}
 
 		// 4) return with correct sign
-		return signed ? -result : result;
+		return signed ? -quotient : quotient;
+	}
+
+	static long divide(long dividend, long divisor) {
+
+		// 1) short-circuit processing and checking
+		if (divisor > dividend) {
+			return 0;
+		}
+		if (divisor == dividend) {
+			return 1;
+		}
+		if (divisor == 0) {
+			throw new ArithmeticException("Division by zero");
+		}
+
+		// 2) handle negatives
+		boolean signed = (dividend > 0) ^ (divisor > 0);
+		if (divisor < 0) {
+			divisor = -divisor;
+		}
+		if (dividend < 0) {
+			dividend = -dividend;
+		}
+
+		// 3) shift 62 times
+		long quotient = 0;
+		long remainder = 0;
+		long bit = 0;
+		
+		for (int i = 63 - 1; i > -1; i--) { // number of bits in positive word - 1
+			remainder <<= 1;
+			bit = (dividend >>> i) & 1; // get the bit i of the dividend
+			remainder |= bit; // set the retrieved bit to the least significant bit of remainder
+			
+			if (remainder >= divisor) {
+				remainder -= divisor;
+				quotient |= 1 << i; // set the ith bit of the quotient to 1
+			}
+		}
+
+		// 4) return with correct sign
+		return signed ? -quotient : quotient;
 	}
 
 	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
 		for (int i = 0; i < Integer.MAX_VALUE / 100; i++) {
+			divide(Integer.MAX_VALUE, 1L);
+		}
+		System.out.println(System.currentTimeMillis() - start + "ms");
+		start = System.currentTimeMillis();
+		for (int i = 0; i < Integer.MAX_VALUE / 100; i++) {
 			divide(Integer.MAX_VALUE, 1);
 		}
-		System.out.println(System.currentTimeMillis() - start);
-		 System.out.println(divide(1, 10));// 0
-		 System.out.println(divide(1, 1));// 1
-		 System.out.println(divide(5, 2));// 2
-		 System.out.println(divide(18, 3));// 6
-		 System.out.println(divide(Integer.MAX_VALUE, 1));// Integer.MAX_VALUE
-		 System.out.println(divide(1, 0));// exception
+		System.out.println(System.currentTimeMillis() - start + "ms");
+		System.out.println(divide(1, 10));// 0
+		System.out.println(divide(1, 1));// 1
+		System.out.println(divide(1, -1));// -1
+		System.out.println(divide(5, 2));// 2
+		System.out.println(divide(18, 3));// 6
+		System.out.println(divide(Integer.MAX_VALUE, 1));// Integer.MAX_VALUE
+		System.out.println(divide(1, 0));// exception
 
 	}
 
