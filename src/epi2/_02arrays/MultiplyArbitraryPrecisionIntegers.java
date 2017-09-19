6818a9346179e8a8cbf0c87152fa63c047f813f8
Christian Rubiales <christianrubiales@yahoo.com>
2017-09-20 01:52:40 +0800

    MultiplyArbitraryPrecisionIntegers

--- src/epi2/_02arrays/MultiplyArbitraryPrecisionIntegers.java
+++ src/epi2/_02arrays/MultiplyArbitraryPrecisionIntegers.java
@@ -18,17 +18,17 @@ public class MultiplyArbitraryPrecisionIntegers {
 		for (int i = x.length - 1; i >= 0; i--) {
 			for (int j = y.length - 1; j >= 0; j--) {
 				result[i+j+1] = result[i+j+1] + x[i] * y[j];
-				result[i+j] = result[i+j] + result[i+j+1] / 10;
+				result[i+j]   = result[i+j]   + result[i+j+1] / 10;
 				result[i+j+1] = result[i+j+1] % 10;
 			}
 		}
 		
 		// remove leading zeroes
-		int index = 0;
-		while (index < result.length && result[index] == 0) {
-			index++;
+		int i = 0;
+		while (i < result.length && result[i] == 0) {
+			i++;
 		}
-		result = Arrays.copyOfRange(result, index, result.length);
+		result = Arrays.copyOfRange(result, i, result.length);
 		if (result.length == 0) {
 			return new Integer[] {0};
 		}
@@ -40,6 +40,7 @@ public class MultiplyArbitraryPrecisionIntegers {
 	}
 
 	public static void main(String[] args) {
+		System.out.println(Arrays.deepToString(multiply(new Integer[] {1,2,3}, new Integer[] {1,0}))); // 1,2,3,0
 		System.out.println(Arrays.deepToString(multiply(new Integer[] {0}, new Integer[] {1}))); // 0
 		System.out.println(Arrays.deepToString(multiply(new Integer[] {1}, new Integer[] {1}))); // 1
 		System.out.println(Arrays.deepToString(multiply(new Integer[] {-1}, new Integer[] {1}))); // -1
