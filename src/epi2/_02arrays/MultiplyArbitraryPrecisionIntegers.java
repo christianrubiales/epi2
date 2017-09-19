da3908efdb1a75976a5ac7b8595562c64548c2eb
Christian Rubiales <christianrubiales@yahoo.com>
2017-09-20 01:12:37 +0800

    MultiplyArbitraryPrecisionIntegers

--- src/epi2/_02arrays/MultiplyArbitraryPrecisionIntegers.java
+++ src/epi2/_02arrays/MultiplyArbitraryPrecisionIntegers.java
@@ -2,20 +2,22 @@ package epi2._02arrays;
 
 import java.util.Arrays;
 
+// big integer multiplication
 public class MultiplyArbitraryPrecisionIntegers {
 	
-	static Integer[] multiply(Integer[] n1, Integer[] n2) {
+	static Integer[] multiply(Integer[] x, Integer[] y) {
+
 		// handle sign
-		int sign = (n1[0] < 0) ^ n2[0] < 0 ? -1 : 1;
-		n1[0] = Math.abs(n1[0]);
-		n2[0] = Math.abs(n2[0]);
+		int sign = (x[0] < 0) ^ y[0] < 0 ? -1 : 1;
+		x[0] = Math.abs(x[0]);
+		y[0] = Math.abs(y[0]);
 		
 		// long multiplication
-		Integer[] result = new Integer[n1.length + n2.length];
+		Integer[] result = new Integer[x.length + y.length];
 		Arrays.fill(result, 0);
-		for (int i = n1.length - 1; i >= 0; i--) {
-			for (int j = n2.length - 1; j >= 0; j--) {
-				result[i+j+1] = result[i+j+1] + n1[i] * n2[j];
+		for (int i = x.length - 1; i >= 0; i--) {
+			for (int j = y.length - 1; j >= 0; j--) {
+				result[i+j+1] = result[i+j+1] + x[i] * y[j];
 				result[i+j] = result[i+j] + result[i+j+1] / 10;
 				result[i+j+1] = result[i+j+1] % 10;
 			}
