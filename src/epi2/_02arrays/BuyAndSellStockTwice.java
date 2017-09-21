bf673d1400d16038e8ca00c682f47e2b5b5b4448
Christian Rubiales <christianrubiales@yahoo.com>
2017-09-22 03:04:11 +0800

    BuyAndSellStockTwice

--- src/epi2/_02arrays/BuyAndSellStockTwice.java
+++ src/epi2/_02arrays/BuyAndSellStockTwice.java
@@ -3,29 +3,38 @@ package epi2._02arrays;
 public class BuyAndSellStockTwice {
 	
 	static Double buyAndSellStockTwice(Double[] prices) {
-		Double maxProfit = 0.0d;
-		Double[] firstBuyAndSellProfits = new Double[prices.length];
 		Double minPriceSoFar = Double.MAX_VALUE;
+		Double maxProfit = 0.0d;
+		Double[] firstProfits = new Double[prices.length];
 		
 		// get max profit possible for each day
 		for (int i = 0; i < prices.length; i++) {
 			minPriceSoFar = Math.min(minPriceSoFar, prices[i]);
 			maxProfit = Math.max(maxProfit, prices[i] - minPriceSoFar);
-			firstBuyAndSellProfits[i] = maxProfit;
+			firstProfits[i] = maxProfit;
 		}
 		
-		// get the max profit if buying again for each day
+		// get the max profit if buying again after previous sell
 		Double maxPriceSoFar = Double.MIN_VALUE;
 		for (int i = prices.length - 1; i > 0; i--) {
+			
+			// going backwards, take note of the max price so far
 			maxPriceSoFar = Math.max(maxPriceSoFar, prices[i]);
-			maxProfit = Math.max(maxProfit, maxPriceSoFar - prices[i] + firstBuyAndSellProfits[i-1]);
+			
+			// maxProfit candidate -> maxPriceSoFar - current price + profit from earlier day
+			maxProfit = Math.max(maxProfit, maxPriceSoFar - prices[i] + firstProfits[i-1]);
 		}
 		
 		return maxProfit;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(buyAndSellStockTwice(new Double[] {12d,11d,13d,9d,12d,8d,14d,13d,15d}));
+		System.out.println(buyAndSellStockTwice(new Double[] {12d,11d,13d,9d,12d,8d,14d,13d,15d})); //10
+		System.out.println(buyAndSellStockTwice(new Double[] {1d,2d,3d,4d})); // 3
+		System.out.println(buyAndSellStockTwice(new Double[] {1d,2d,3d,2d,4d})); // 4
+		System.out.println(buyAndSellStockTwice(new Double[] {1d,2d,4d,2d,3d,2d,4d})); // 5
+		System.out.println(buyAndSellStockTwice(new Double[] {2d,30d,15d,10d,8d,25d,80d})); // 100
+		System.out.println(buyAndSellStockTwice(new Double[] {100d,30d,15d,10d,8d,25d,80d})); // 72
 	}
 
 }
