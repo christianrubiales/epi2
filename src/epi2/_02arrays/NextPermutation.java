ef2d6b7ea0e85087d5854e7dd90249ba1e80a14e
Christian Rubiales <christianrubiales@yahoo.com>
2017-09-22 03:46:42 +0800

    NextPermutation

--- src/epi2/_02arrays/NextPermutation.java
+++ src/epi2/_02arrays/NextPermutation.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class NextPermutation {
 	
 	static Integer[] nextPermutation(Integer[] A) {
-		// 1) handle arrays with less than two items
+		// 1) handle arrays with less than two entries
 		if (A == null || A.length < 2) {
 			return null;
 		}
@@ -19,20 +19,20 @@ public class NextPermutation {
 			i--;
 		}
 		
-		// 3) return null if last permutation already reached
+		// 3) if already the last permutation, return null
 		if (i == -1) {
 			return null;
 		}
 		
 		// 4) find rightmost entry greater than A[i], then swap
 		for (int j = A.length - 1; j > i; j--) {
-			if (A[i] < A[j]) {
+			if (A[j] > A[i]) {
 				swap(A, i, j);
 				break;
 			}
 		}
 		
-		// 5) reverse the descending sequence
+		// 5) reverse from A[i+1] to end
 		reverse(A, i+1, A.length - 1);
 		
 		return A;
@@ -50,7 +50,7 @@ public class NextPermutation {
 			i--;
 		}
 		
-		// 3) first permutation already reached
+		// 3) if last permutation already reached, return null
 		if (i == -1) {
 			return null;
 		}
@@ -63,7 +63,7 @@ public class NextPermutation {
 			}
 		}
 		
-		// 5) reverse the descending sequence
+		// 5) reverse the descending sequence, from A[i+1] to end
 		reverse(A, i+1, A.length - 1);
 		
 		return A;
@@ -96,6 +96,7 @@ public class NextPermutation {
 		
 		System.out.println(Arrays.deepToString(previousPermutation(new Integer[] {1,2,0,3}))); // 1,0,3,2
 		System.out.println(Arrays.deepToString(previousPermutation(new Integer[] {0,1,2,3}))); // null
+		System.out.println(Arrays.deepToString(previousPermutation(new Integer[] {0,1,3,2}))); // 0,1,2,3
 
 	}
 
