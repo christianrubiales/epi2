6936ee8ab0dbec8c7136897dd0f1cb57b981d609
Christian Rubiales <christianrubiales@yahoo.com>
2017-09-27 01:58:40 +0800

    HeapsPermutationIterative

--- src/epi2/_02arrays/HeapsPermutationIterative.java
+++ src/epi2/_02arrays/HeapsPermutationIterative.java
@@ -1,16 +1,29 @@
 package epi2._02arrays;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HeapsPermutationIterative {
 	
+	static Set<String> set = new HashSet<>();
+	
 	static void generate(char[] A) {
+//		set.clear();
 		int[] c = new int[A.length];
 		System.out.println(A);
+//		set.add(new String(A));
 		
 		int i = 0;
 		while (i < A.length) {
 			if (c[i] < i) {
-				swap(A, 0, i);
+				if (i % 2 == 0) {
+					swap(A, 0, i);
+				} else {
+					swap(A, c[i], i);
+				}
+				
 				System.out.println(A);
+//				set.add(new String(A));
 				
 				c[i]++;
 				i = 0;
@@ -19,6 +32,7 @@ public class HeapsPermutationIterative {
 				i++;
 			}
 		}
+//		System.out.println(set.size());
 	}
 	
 	static void swap(char[] array, int i, int j) {
@@ -28,10 +42,10 @@ public class HeapsPermutationIterative {
 	}
 
 	public static void main(String[] args) {
-		generate("ABC".toCharArray());
+//		generate("ABC".toCharArray());
 		generate("123".toCharArray());
-		generate("1234".toCharArray());
+//		generate("1234".toCharArray());
 		generate("ABCD".toCharArray());
-		generate("112".toCharArray());
+//		generate("112".toCharArray());
 	}
 }
