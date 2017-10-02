eb8db77b8904a79dcca24edae89d37a9668c4c30
Christian Rubiales <christianrubiales@yahoo.com>
2017-10-03 03:38:45 +0800

    SpiralOrderOfMatrix

--- src/epi2/_02arrays/SpiralOrderOfMatrix.java
+++ src/epi2/_02arrays/SpiralOrderOfMatrix.java
@@ -66,6 +66,14 @@ public class SpiralOrderOfMatrix {
 			{1,2,3,4},
 			{5,6,7,8},
 			{9,10,11,12},
+			{13,14,15,16}
+		};
+		System.out.println(spiralOrder(M));
+		
+		M = new Integer[][] {
+			{1,2,3,4},
+			{5,6,7,8},
+			{9,10,11,12},
 			{13,14,15,16},
 			{17,18,19,20}
 		};
