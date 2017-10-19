01633f6d6da181aedad3112d9807b2453e99bb2a
Christian Rubiales <christianrubiales@yahoo.com>
2017-10-20 03:43:55 +0800

    RotateNonSquareMatrix

---
+++ src/epi2/_02arrays/RotateNonSquareMatrix.java
@@ -0,0 +1,40 @@
+package epi2._02arrays;
+
+import java.util.Arrays;
+//import java.util.Random;
+
+public class RotateNonSquareMatrix {
+	
+	static Integer[][] rotateClockwise(Integer[][] M) {
+		Integer[][] N = new Integer[M[0].length][M.length];
+		
+		for (int i = 0; i < M.length; i++) {
+			for (int j = 0; j < M[i].length; j++) {
+				N[j][M.length - i - 1] = M[i][j];
+			}
+		}
+		
+		return N;
+	}
+	
+	static Integer[][] rotateCounterclockwise(Integer[][] M) {
+		Integer[][] N = new Integer[M[0].length][M.length];
+		
+		for (int i = 0; i < M.length; i++) {
+			for (int j = 0; j < M[i].length; j++) {
+				N[M[i].length - j - 1][i] = M[i][j];
+			}
+		}
+		
+		return N;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.deepToString(rotateClockwise(new Integer[][] {{1,2,3},{4,5,6}})));
+		System.out.println(Arrays.deepToString(rotateClockwise(new Integer[][] {{1,2,3,4},{5,6,7,8},{9,10,11,12}})));
+		
+		System.out.println(Arrays.deepToString(rotateCounterclockwise(new Integer[][] {{1,2,3},{4,5,6}})));
+		System.out.println(Arrays.deepToString(rotateCounterclockwise(new Integer[][] {{1,2,3,4},{5,6,7,8},{9,10,11,12}})));
+	}
+
+}
