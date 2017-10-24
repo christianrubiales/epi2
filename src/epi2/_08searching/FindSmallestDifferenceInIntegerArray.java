f63b3284f31561a6af5f31adcbca55b6a7eb6d5b
Christian Rubiales <christianrubiales@yahoo.com>
2017-10-25 03:58:17 +0800

    FindSmallestDifferenceInIntegerArray

---
+++ src/epi2/_08searching/FindSmallestDifferenceInIntegerArray.java
@@ -0,0 +1,23 @@
+package epi2._08searching;
+
+import java.util.Arrays;
+
+public class FindSmallestDifferenceInIntegerArray {
+	
+	static int smallestDifference(int[] A) {
+		Arrays.sort(A);
+		int smallest = Integer.MAX_VALUE;
+		
+		for (int i = 0; i < A.length - 1; i++) {
+			smallest = Math.min(smallest, A[i+1] - A[i]);
+		}
+		
+		return smallest;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(smallestDifference(new int[] {2,4,6,7,8,10}));//1
+		System.out.println(smallestDifference(new int[] {10,9,8,6,4,2}));//1
+	}
+
+}
