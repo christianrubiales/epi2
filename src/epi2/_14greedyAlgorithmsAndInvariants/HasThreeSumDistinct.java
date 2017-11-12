47d382d947fbd96c1b77f7c1277b11fb0cb9ca68
Christian Rubiales <christianrubiales@yahoo.com>
2017-11-12 22:49:33 +0800

    HasThreeSumDistinct

--- src/epi2/_14greedyAlgorithmsAndInvariants/HasThreeSumDistinct.java
+++ src/epi2/_14greedyAlgorithmsAndInvariants/HasThreeSumDistinct.java
@@ -2,26 +2,31 @@ package epi2._14greedyAlgorithmsAndInvariants;
 
 import java.util.Arrays;
 
-// Valid only for non-negative inputs
-// O(nlogn)
-// FIXME
+// @see https://en.wikipedia.org/wiki/3SUM#Quadratic_algorithm
 public class HasThreeSumDistinct {
 	
 	static boolean hasThreeSum(int[] A, int sum) {
-		Arrays.sort(A);
-		int i = 0;
-		int j = A.length - 1;
+		if (A == null || A.length < 3) {
+			return false;
+		}
 		
-		while (i < j) {
-			int c = Arrays.binarySearch(A, i+1, j, sum - (A[i]+A[j]));
-			if (c > -1) {
-//				System.out.println(A[i] + "+" + A[j] + "+" + A[c] + "=" + sum);
-				return true;
-			}
-			if (A[i] + A[j] <= sum) {
-				i++;
-			} else if (A[i] + A[j] > sum) {
-				j--;
+		Arrays.sort(A);
+		for (int i = 0; i < A.length - 2; i++) {
+			int a = A[i];
+			int start = i+1;
+			int end = A.length - 1;
+			
+			while (start < end) {
+				int b = A[start];
+				int c = A[end];
+				
+				if (a+b+c == sum) {
+					return true;
+				} else if (a+b+c > sum) {
+					end--;
+				} else {
+					start++;
+				}
 			}
 		}
 		
@@ -29,8 +34,8 @@ public class HasThreeSumDistinct {
 	}
 
 	public static void main(String[] args) {
-		int[] A = new int[] {0,1,2,3};
-		for (int i = -1; i < 10; i++) {
+		int[] A = new int[] {-3,-2,-1,0,1,2,3};
+		for (int i = -10; i < 10; i++) {
 			System.out.println(i + " : " + hasThreeSum(A, i));
 		}
 	}
