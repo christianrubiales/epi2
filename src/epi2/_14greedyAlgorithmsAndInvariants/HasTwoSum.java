7cf4231ead5821bf1a674b13358bf3fb7417828a
Christian Rubiales <christianrubiales@yahoo.com>
2017-11-12 20:26:00 +0800

    HasTwoSum

--- src/epi2/_14greedyAlgorithmsAndInvariants/HasTwoSum.java
+++ src/epi2/_14greedyAlgorithmsAndInvariants/HasTwoSum.java
@@ -2,6 +2,7 @@ package epi2._14greedyAlgorithmsAndInvariants;
 
 import java.util.Arrays;
 
+// O(n^2)
 public class HasTwoSum {
 	
 	static boolean hasTwoSum(int[] A, int sum) {
@@ -10,8 +11,9 @@ public class HasTwoSum {
 		int i = 0;
 		int j = A.length - 1;
 		
-		while (i < j) {
+		while (i <= j) {
 			if (A[i] + A[j] == sum) {
+//				System.out.println(A[i] + "+" + A[j] + "=" + sum);
 				return true;
 			} else if (A[i] + A[j] < sum) {
 				i++;
@@ -22,11 +24,26 @@ public class HasTwoSum {
 		
 		return false;
 	}
+	
+	static boolean hasThreeSum(int[] A, int sum) {
+		for (int a : A) {
+			if (hasTwoSum(A, sum - a)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
 
 	public static void main(String[] args) {
-		int[] A = new int[] {1,2,3,4,5,6,7,8,9};
-		for (int i = 0; i < 10; i++) {
+		int[] A = new int[] {-3,-2,-1,0,1,2,3};
+		for (int i = -10; i < 10; i++) {
 			System.out.println(i + " : " + hasTwoSum(A, i));
+		}
+		System.out.println();
+		
+		for (int i = -10; i < 13; i++) {
+			System.out.println(i + " : " + hasThreeSum(A, i));
 		}
 	}
 
